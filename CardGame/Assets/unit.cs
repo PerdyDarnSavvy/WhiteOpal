57a705d1def3c700218cbe5000e2be73581bc12b
Alex <38225565+PerdyDarnSavvy@users.noreply.github.com>
2018-04-10 07:44:23 -0500

    Create unit.cs

---
+++ CardGame/Assets/unit.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unit : MonoBehaviour {
+
+	protected int health;
+	protected int damage;
+	  
+  public void Attack () {
+  
+  }
+	
+  public virtual void Action () {
+  
+  }
+  
+  public void Die () {
+  
+  }
+  
+}
